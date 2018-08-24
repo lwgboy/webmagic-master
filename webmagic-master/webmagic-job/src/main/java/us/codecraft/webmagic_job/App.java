423f3543f8e888265accb4a151401a00cdd9a637
xiongyangyang <525210957@qq.com>
2018-08-24 14:08:36 +0800

    提交

---
+++ webmagic-master/webmagic-job/src/main/java/us/codecraft/webmagic_job/App.java
@@ -0,0 +1,13 @@
+package us.codecraft.webmagic_job;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
